ec7f4a10416cf85022452b034b2eaf31fbf0d688
ncaillet <ncaillet@insa-toulouse.fr>
2024-11-13 12:10:19 +0100

    conversion binary

--- emetteur.py
+++ emetteur.py
@@ -1,13 +1,16 @@
-type_trame = '170'
+#Trame données de présenceA vers PC Central
 
-add_dest = ''
+type_trame = b'00000'
 
-add_src = ''
+add_dest = b'111011000101010111111001111110111011111011101110'
+
+add_src = b'111011000101010111111001001010001000111100010000'
 
 f = open("capture_test.txt", "r")
 data = f.read()
+#convertir data en binary string
 
-crc = '000'
+crc = b'000'
 
 
 trame = type_trame + add_dest + add_src + data + crc
@@ -20,8 +23,24 @@ trame_crc = trame
 
 
 
+print(trame_crc)
 
-a = 'Ob10101010'
+
+a = '10101010'
 b = '0b11111111'
-c = a+b
-print(a+b)
+#c = bin(int(a))
+
+data = (''.join(format(ord(x), 'b') for x in a))
+print(data)
+print(type(data))
+
+byte_string = b'10101010'
+string = byte_string.decode('utf-8')
+print(byte_string)
+print(string)
+print(type(byte_string))
+print(type(string))
+
+# a) créer l'octet en bytes avec b'xxxxxxxx'
+# b) concaténer avec un +
+# c) si besoin, trame.decode('utf-8')
